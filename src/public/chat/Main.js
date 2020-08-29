9bbbcce9d7fae4b76b874bc484ae51e1820c1898
LookPlayEspYT <60762375+LookPlayEspYT@users.noreply.github.com>
2020-08-29 09:31:15 -0700

    chat v0.1 v0.1.0

--- src/public/chat/Main.js
+++ src/public/chat/Main.js
@@ -1,1 +1,23 @@
 const Socket = io();
+
+const UserInput = document.querySelector('#input');
+const Msgs = document.querySelector('.msgs');
+// const _ = document.querySelector('');
+
+UserInput.onchange = e => {
+  Emit('console', UserInput.value);
+}
+
+Socket.on('evt', Data => {
+  if(Data.type === 'console'){
+    console.log(Data.msg);
+  }
+});
+
+function Emit(type, msg) {
+  Socket.emit('evt', {
+    type: type,
+    msg: msg,
+    id: Socket.id
+  });
+}
