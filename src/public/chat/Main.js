b589ce1c2cec9f76ea7cabfb7a63eca21931531a
LookPlayEspYT <60762375+LookPlayEspYT@users.noreply.github.com>
2020-08-30 16:02:37 -0700

    chat v0.7 v0.1.7

--- src/public/chat/Main.js
+++ src/public/chat/Main.js
@@ -1,8 +1,25 @@
 const Socket = io();
 
+const UI = {
+  openChat: true
+};
 const UserInput = document.querySelector('#input');
 const Msgs = document.querySelector('.msgs');
+UI.ArrowBtn = document.querySelector('.arrow');
+UI.Main = document.querySelector('main');
 // const _ = document.querySelector('');
+
+// UI
+UI.ArrowBtn.onclick = () => {
+  if(UI.openChat){
+    UI.ArrowBtn.style.transform = "rotate(0deg)";
+    UI.Main.style.height = "5em";
+  }else{
+    UI.ArrowBtn.style.transform = "rotate(180deg)";
+    UI.Main.style.height = "30em";
+  }
+  UI.openChat = !UI.openChat;
+}
 
 UserInput.onchange = e => {
   Emit('console', UserInput.value);
